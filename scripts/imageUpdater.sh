6d84b414133cce8215a123b0ca86fd38e334340a
Baby K <azkdev92@gmail.com>
2025-01-21 19:14:58 +0000

    Updated imageUpdater.sh

--- scripts/imageUpdater.sh
+++ scripts/imageUpdater.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-set -x
-
-REPO_URL="https://B9FeDCntv6BF8ipN6VWBDM5otB9L0QrHfTIb0Cr7Hf9nYuGIUhAZJQQJ99BAACAAAAAAAAAAAAASAZDOzfwN@dev.azure.com/azkdev992/K%20Dev/_git/K%20Dev"
-
-git clone "REPO_URL" /tmp/temp_repo
-cd tmp/temp_repo
-
-sed -i "s|image:.*|image: azdevreg.azurecr.io/kdev:30|g" k8smanifests/deployment.yml
-
-git add .
-git commit -m "Updated the K8S file with new image tag"
-git push
-rm -rf /tmp/temp_repo
+echo "hi"
