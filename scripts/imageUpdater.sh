47acbc2ea5976f1540990a534eafa4a5f6a17d93
Baby K <azkdev92@gmail.com>
2025-01-21 17:36:42 +0000

    Updated imageUpdater.sh

--- scripts/imageUpdater.sh
+++ scripts/imageUpdater.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-#set -x
+set -x
+
+set -e
 
 # Set the repository URL
 REPO_URL="https://58UaIMznNKDqTrd7Oizcmj1j6UfJiLyNS4dovgg46sXs46vIa4EIJQQJ99BAACAAAAAAAAAAAAASAZDOe1Tq@dev.azure.com/azkdev992/K%20Dev/_git/K%20Dev"
-
+TEMP_DIR="/tmp/temp_repo"
 # Clone the git repository into the /tmp directory
-git clone "$REPO_URL" /tmp/temp_repo
+git clone "$REPO_URL" "$TEMP_DIR"
 
 # Navigate into the cloned repository directory
-cd /tmp/temp_repo
+cd "$TEMP_DIR"
 
 # Make changes to the Kubernetes manifest file(s)
+if [[ ! -f "k8smanifests/deployment.yml" ]]; then
+  echo "Error: deployment.yml not found in k8smanifests directory."
+  exit 1
+fi
 # For example, let's say you want to change the image tag in a deployment.yaml file
 sed -i "s|image:.*|image: azdevreg.azurecr.io/kdev:$1|g" k8smanifests/deployment.yml
 
 # Add the modified files
-git add .
+git add k8smanifests/deployment.yml
 
 # Commit the changes
-git commit -m "Update Kubernetes manifest"
+git commit -m "Update Kubernetes manifest with new image tag $1"
 
 # Push the changes back to the repository
 git push
 
 # Cleanup: remove the temporary directory
-rm -rf /tmp/temp_repo
+rm -rf "$TEMP_DIR"
